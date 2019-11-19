71a9f667f1ac900feb2780d0d44b9f9f31b994f0
kmnam <kmnam@g.harvard.edu>
2019-11-19 13:54:08 -0500

    First commit

---
+++ include/polynomial.hpp
@@ -0,0 +1,287 @@
+#ifndef UNIVAR_POLYNOMIAL_HPP
+#define UNIVAR_POLYNOMIAL_HPP
+
+#include <utility>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <Eigen/Dense>
+#include "complex/complex.hpp"
+
+/*
+ * A Polynomial class template with arbitrary real coefficients.
+ *
+ * The coefficients used in this class should be those that support usage
+ * with std::complex.  
+ *
+ * Authors:
+ *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
+ * Last updated:
+ *     11/18/2019
+ */
+using namespace Eigen;
+
+// ------------------------------------------------------------------ //
+//                      VARIOUS HELPER FUNCTIONS                      //
+// ------------------------------------------------------------------ //
+const double two_pi = 2.0 * std::acos(-1);
+
+template <typename T>
+Matrix<T, Dynamic, 1> appendZeros(const Ref<Matrix<T, Dynamic, 1> >& v, unsigned j)
+{
+    /*
+     * Pad the given vector, v, with j zeros. 
+     */
+    Matrix<T, Dynamic, 1> w(v);
+    w.conservativeResize(w.size() + j);
+    for (unsigned i = w.size(); i < w.size() + j; ++i) w(i) = 0;
+    return w;
+}
+
+template <typename T>
+class Polynomial
+{
+    private:
+        unsigned deg;                    // Degree of the polynomial
+        Matrix<T, Dynamic, 1> coefs;     // Coefficients of the polynomial,
+                                         // stored in ascending order of power
+
+    public:
+        Polynomial()
+        {
+            /*
+             * Empty constructor for the zero polynomial.
+             */
+            this->deg = 0.0;
+            this->coefs.resize(1);
+            this->coefs << 0.0;
+        }
+
+        Polynomial(const Ref<const Matrix<T, Dynamic, 1> >& coefs)
+        {
+            /*
+             * Constructor with user-specified coefficients.
+             */
+            this->deg = coefs.size() - 1;
+            this->coefs = coefs;
+        }
+
+        ~Polynomial()
+        {
+            /*
+             * Empty destructor.
+             */
+        }
+
+        unsigned degree() const
+        {
+            /*
+             * Return the degree of this polynomial.
+             */
+            return this->deg;
+        }
+
+        Matrix<T, Dynamic, 1> coefficients() const
+        {
+            /*
+             * Return the coefficients of this polynomial.
+             */
+            return this->coefs;
+        }
+
+        T eval(T x) const
+        {
+            /*
+             * Return the value obtained by evaluating the polynomial at x.
+             *
+             * Horner's method: starting with the last (highest-degree)
+             * coefficient, recursively do the following:
+             * 1) Multiply by x
+             * 2) Add by the previous coefficient
+             */
+            T y = this->coefs(this->deg);
+            for (unsigned i = this->deg - 1; i >= 0; ++i)
+            {
+                y *= x;
+                y += this->coefs(i);
+            }
+            return y;
+        }
+
+        std::complex<T> eval(std::complex<T> x) const
+        {
+            /*
+             * Return the value obtained by evaluating the polynomial at x
+             * with Horner's method.
+             */
+            std::complex<T> y(this->coefs(this->deg), 0.0);
+            for (unsigned i = this->deg - 1; i >= 0; ++i)
+            {
+                y *= x;
+                y += this->coefs(i);
+            }
+            return y;
+        }
+
+        Matrix<T, Dynamic, 1> eval(const Ref<const Matrix<T, Dynamic, 1> >& x) const
+        {
+            /*
+             * Return the vector of values obtained by evaluating the polynomial
+             * at each coordinate of the vector x.
+             */
+            Array<T, Dynamic, 1> y = Array<T, Dynamic, 1>::Constant(x.size(), 1, this->coefs(this->deg));
+            for (unsigned i = this->deg - 1; i >= 0; ++i)
+            {
+                y *= x.array();
+                y += this->coefs(i);
+            }
+            return y.matrix();
+        }
+
+        Polynomial operator+(const Polynomial<T>& q) const 
+        {
+            /*
+             * Return the result of adding by q. 
+             */
+            // Copy over the coefficients into new vectors and resize 
+            // as necessary 
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            Matrix<T, Dynamic, 1> q_coefs(q.coefficients());
+            if (this->deg > q.degree())
+                q_coefs = appendZeros<T>(q_coefs, q.degree() - this->deg);
+            else if (this->deg < q.degree())
+                p_coefs = appendZeros<T>(p_coefs, this->deg - q.degree());
+
+            // Instantiate the sum polynomial
+            return Polynomial(p_coefs + q_coefs);
+        }
+
+        Polynomial operator+(const T s) const
+        {
+            /*
+             * Return the result of adding by scalar s.
+             */
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            p_coefs(0) += s;
+            return Polynomial(p_coefs);
+        }
+
+        Polynomial operator-(const Polynomial<T>& q) const
+        {
+            /*
+             * Return the result of subtracting by q. 
+             */
+            // Copy over the coefficients into new vectors and resize 
+            // as necessary 
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            Matrix<T, Dynamic, 1> q_coefs(q.coefficients());
+            if (this->deg > q.degree())
+                q_coefs = appendZeros<T>(q_coefs, q.degree() - this->deg);
+            else if (this->deg < q.degree())
+                p_coefs = appendZeros<T>(p_coefs, this->deg - q.degree());
+
+            // Instantiate the difference polynomial
+            return Polynomial(p_coefs - q_coefs);
+        }
+
+        Polynomial operator-(const T s) const
+        {
+            /*
+             * Return the result of subtracting by scalar s.
+             */
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            p_coefs(0) -= s;
+            return Polynomial(p_coefs);
+        }
+
+        Polynomial operator*(const Polynomial<T>& q) const
+        {
+            /*
+             * Return the result of multiplying by q.
+             *
+             * Compute the product with q using the FFT algorithm:
+             * 1) Evaluate the two polynomials at the (2d + 2)-th roots of 
+             *    unity, where d = max(this->deg, q.degree).
+             * 2) Multiply the pairs of values obtained above.
+             * 3) Interpolate the pairs of values to obtain the product
+             *    polynomial. 
+             */
+            unsigned p_deg = this->degree;
+            unsigned q_deg = q.degree();
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            Matrix<T, Dynamic, 1> q_coefs = q.coefficients();
+
+            // Evaluate the two polynomials at the (2d)-th roots of unity
+            Array<std::complex<T>, Dynamic, 1> values_p(2 * max_deg);
+            Array<std::complex<T>, Dynamic, 1> values_q(2 * max_deg);
+            std::vector<Comp<T> > values_p, values_q;
+            for (unsigned i = 0; i < 2 * max_deg; ++i)
+            {
+                T a = std::cos(two_pi * i / (2 * max_deg));
+                T b = std::sin(two_pi * i / (2 * max_deg));
+                std::complex<T> z(a, b);
+                values_p(i) = this->eval(z);
+                values_q(i) = q.eval(z);
+            }
+
+            // Multiply the two vectors of values pointwise
+            auto values_pq = (values_p * values_q).matrix();
+
+            // Interpolate the product values by the inverse DFT
+            Matrix<std::complex<T>, Dynamic, Dynamic> inv_dft(2 * max_deg, 2 * max_deg);
+            for (unsigned i = 0; i < 2 * max_deg; ++i)
+            {
+                inv_dft(0, i) = 1.0;
+                inv_dft(i, 0) = 1.0;
+            }
+            for (unsigned i = 1; i < 2 * max_deg; ++i)
+            {
+                for (unsigned j = 1; j < 2 * max_deg; ++j)
+                {
+                    T a = std::cos(two_pi * i * j / (2 * max_deg));
+                    T b = -std::sin(two_pi * i * j / (2 * max_deg));
+                    std::complex<T> z(a, b);
+                    inv_dft(i, j) = z; 
+                }
+            }
+            Matrix<T, Dynamic, 1> prod_coefs = inv_dft * values_pq;
+            return Polynomial(prod_coefs);
+        }
+
+        Polynomial operator*(const T s) const
+        {
+            /*
+             * Return the result of multiplying by scalar s.
+             */
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            p_coefs *= s;
+            return Polynomial(p_coefs);
+        }
+
+        Polynomial operator/(const T s) const
+        {
+            /*
+             * Return the result of dividing by scalar s.
+             */
+            Matrix<T, Dynamic, 1> p_coefs(this->coefs);
+            p_coefs /= s;
+            return Polynomial(p_coefs);
+        }
+
+        Polynomial diff() const 
+        {
+            /*
+             * Return the derivative of this polynomial.
+             */
+            if (this->degree == 0)
+                return Polynomial();
+
+            Matrix<T, Dynamic, 1> dcoefs = (
+                Array<T, Dynamic, 1>::LinSpaced(this->degree, 1, this->degree)
+                * this->coefs.tail(this->degree).array()
+            ).matrix();
+            return Polynomial(dcoefs);
+        }
+};
+#endif
